add3845c296458236c00f7434ffcd1c071d4fe7c
Sachin <soniji7269@gmail.com>
2022-09-13 23:01:29 +0530

    Code_by_Sachin

--- primeNumber.js
+++ primeNumber.js
@@ -1,4 +1,16 @@
 // find prime number.
 let num=17;
 prime=true;
-for (let i=0;i<num)
+
+
+for (let i=2;i<num; i++){
+    if (num%i===0){
+        prime=false;
+    }
+}
+
+if (prime===true){
+    console.log("Prime no");
+} else {
+    console.log("Not prime");
+}
